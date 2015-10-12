c81423c1ca8510f1fa9f5fe8f99bfc3e3a863c8e
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2015-10-12 13:22:00 +0100

    Update for 2015
    Update for 2015

--- training/workshops_2015/linux_workshop/slides/login.sh
+++ training/workshops_2015/linux_workshop/slides/login.sh
@@ -1,7 +1,5 @@
-# step 1 login to zuserver
+# step 1: login to zuserver
 ssh -YC username@zuserver.star.ucl.ac.uk
 
-# step 2 login to other machines from
-# zuserver
-
+# step 2: login to other machines from zuserver
 ssh -YC username@splinter-login.star.ucl.ac.uk
