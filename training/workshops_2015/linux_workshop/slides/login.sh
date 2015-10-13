15e7b342b118148e1a3eeb3b6db98dd9cccc51f8
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2015-10-13 10:07:50 +0100

    Minor formatting
    Minor formatting

--- training/workshops_2015/linux_workshop/slides/login.sh
+++ training/workshops_2015/linux_workshop/slides/login.sh
@@ -1,5 +1,5 @@
-# step 1: login to zuserver1
+# Step 1: login to zuserver1
 ssh -YC username@zuserver1.star.ucl.ac.uk
 
-# step 2: login to splinter (or other machines) from zuserver1
+# Step 2: login to splinter (or other machines) from zuserver1
 ssh -YC username@splinter-login.star.ucl.ac.uk
